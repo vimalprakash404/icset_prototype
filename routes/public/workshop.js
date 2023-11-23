eb076cbe9037ddaa5950084451ea65bbabcf6cf4
vimalprakash404 <vimalprakash332@gmail.com>
2023-11-23 20:07:38 +0530

    data added

--- routes/public/workshop.js
+++ routes/public/workshop.js
@@ -9,4 +9,10 @@ router.get("/get/:event",async (req , res) => {
     return res.status(200).json(data);
 })
 
+
+router.post("/verify", async (req , res) => {
+    const {event,userId} = req.body;
+    res.status(200).json({"message" : event , userId })
+})
+
 module.exports = router;

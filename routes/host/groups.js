28c1fc63f3571045615be9929f3a6d7b3b69caa9
vimalprakash404 <vimalprakash332@gmail.com>
2024-01-09 09:42:14 +0530

    form added

---
+++ routes/host/groups.js
@@ -0,0 +1,117 @@
+const express = require("express")
+const db = require("../../db/connection")
+const router = express.Router()
+const { Groups } = require("../../models/group")
+const event_model = require("../../models/event")
+const { body, validationResult } = require("express-validator");
+
+
+function checkCollectionExists(collectionName) {
+  return db.connection.db.listCollections({ name: collectionName }).next((err, collinfo) => {
+    if (err) {
+      console.error(`Error checking collection: ${err}`);
+      return false;
+    }
+    else {
+      return true;
+    }
+  });
+}
+
+
+router.get("/:eventId", async (req, res) => {
+  const { eventId } = req.params
+  if ((await checkCollectionExists("group_" + eventId)) === null) {
+    return res.status(404).json({ message: "event not found" })
+  }
+  else {
+    return res.status(200).json({ "data": await Groups("group_" + eventId).find({}) })
+  }
+})
+
+const group_validator = [
+  body("name").isString().notEmpty(),
+  body("event").isMongoId(),
+];
+
+const edit_group_validator = [
+  body("name").isString().notEmpty(),
+  body("event").isMongoId(),
+  body("id").isMongoId()
+];
+
+
+router.post("/add", group_validator, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  else {
+    if (await event_model.findOne({ _id: req.body.event }).exists) {
+      const data_model = Groups("group_" + req.body.event)
+      const group = new data_model(req.body);
+      group.save();
+      return res.status(200).json({ message: "data saved" })
+    }
+    else {
+      return res.status(404).json({ message: "event not found" })
+    }
+
+  }
+});
+
+router.post("/edit", edit_group_validator, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  else {
+    if (await event_model.findOne({ _id: req.body.event }).exists) {
+      const data_model = Groups("group_" + req.body.event)
+      console.log(await data_model.find({}))
+      console.log(await data_model.findByIdAndUpdate(req.body.id,
+        req.body,
+        { new: true }
+      ))
+
+      return res.status(200).json({ message: "data saved" })
+    }
+    else {
+      return res.status(404).json({ message: "event  found" })
+    }
+
+  }
+})
+
+const delete_group_is_validator  = [
+  body("id").isMongoId(),
+  body("event").isMongoId()
+]
+
+
+router.post("/delete" , delete_group_is_validator,async (req , res) => {
+  const error = validationResult(req)
+  if (!error.isEmpty())
+  {
+    return res.status(400).json({error : error.array()})
+  }
+  if (await event_model.findOne({_id : req.body.id}).exists){
+    if ((await checkCollectionExists("group_"+req.body.event))!==null){
+      const data_model = Groups("group_" + req.body.event)
+      await data_model.findOneAndDelete(req.params.id)
+      return res.status(200).json({"message" : "deleted"})
+    }
+    else {
+      return res.status(404).json({"message" : "group not found"})
+    }
+  }
+  else{
+    return res.status(404).json({"message" : "event not found"})
+  }
+})
+
+
+
+
+
+module.exports = router
--- routes/host/host.js
+++ routes/host/host.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const event = require("./event")
 const {workshop_router} = require("./workshop");
 const form_router = require("./form")
+const group = require("./groups")
 const { ReadPreference } = require("mongodb");
 const dashboard  = require("./dashboard")
 router.use("/dashboard",dashboard);
 router.use("/event",event)
 router.use("/workshop",workshop_router);
 router.use("/form",form_router);
+router.use("/group", group);
 module.exports = router ;
